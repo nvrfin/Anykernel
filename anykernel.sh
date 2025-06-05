eb2da5dcb1585bab960d63897e93504f1e316dae
Husam Alhayek <eng.termnh@gmail.com>
2025-06-05 12:13:12 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,96 +4,34 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=AkameKernel for A15 ( EXPERIMENTAL )
+kernel.string=AkameKernel for Android 12-14 ( ginkgo )
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=1
 device.name1=ginkgo
-device.name2=xiaomiredminote8
-supported.versions=
+device.name2=ginkgoin
+device.name3=willow
+supported.versions=12-14
 supported.patchlevels=
 '; } # end properties
 
-isTimRom() {
-  build_prop="/system/build.prop"
-  version_prop="ro.crdroid.build.version"
-
-  if grep -q "$version_prop" "$build_prop" && grep -q "timjosten" "$build_prop"; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-isDerp() {
-  build_prop="/system/build.prop"
-  version_prop="ro.derp.fingerprint"
-
-  if grep -q "$version_prop" "$build_prop" && grep -q "DerpFest" "$build_prop"; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-# Initialize block
+# Initialize block for non-dynamic partitions
 block=/dev/block/bootdevice/by-name/boot;
 
-if isTimRom; then
-    block=boot;
-    is_slot_device=auto;
-    ramdisk_compression=none;
+# AOSP configuration for Android 12-14 non-dynamic
+is_slot_device=0;
+ramdisk_compression=auto;
+no_block_display=true;
 
-    . tools/ak3-core.sh;
+. tools/ak3-core.sh;
 
-    ui_print " » Custom ROM recognition: Tim's Signature ";
-    ui_print " » Initiating vibration fix deployment "
-    ui_print " » Executing AkameKernel flash....";
+ui_print " » Target: AOSP Android 12-14 Non-Dynamic ";
+ui_print " » Installing AkameKernel for ginkgo...";
 
-    split_boot;
-    patch_cmdline initcall_blacklist initcall_blacklist=
-    flash_boot;
+dump_boot;
+write_boot;
 
-elif isDerp; then
-
-    block=/dev/block/by-name/boot;
-    is_slot_device=0;
-    ramdisk_compression=auto;
-    no_block_display=true;
-
-    . tools/ak3-core.sh;
-
-    mount -o rw,remount -t auto /vendor >/dev/null;
-    restore_file /vendor/etc/init/hw/init.target.rc;
-
-    rm -rf $ramdisk/overlay;
-    rm -rf $ramdisk/overlay.d;
-
-    ui_print " » Custom ROM recognition: DerpFest's detected ";
-    ui_print " » Executing AkameKernel flash....";
-
-    dump_boot;
-    write_boot;
-
-else
-
-    is_slot_device=0;
-    ramdisk_compression=auto;
-    no_block_display=true;
-
-    . tools/ak3-core.sh;
-
-    mount -o rw,remount -t auto /vendor >/dev/null;
-    restore_file /vendor/etc/init/hw/init.target.rc;
-
-    rm -rf $ramdisk/overlay;
-    rm -rf $ramdisk/overlay.d;
-
-    ui_print " » Executing AkameKernel flash....";
-
-    dump_boot;
-    write_boot;
-
-fi
+ui_print " » Kernel installation completed successfully! ";
+ui_print " » Compatible: AOSP Android 12-14 ";
